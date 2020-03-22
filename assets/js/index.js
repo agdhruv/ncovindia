f270fe827201fdb7850df16169e95a5d73c981d8
agdhruv <dhruv_agarwal@live.com>
2020-03-22 16:37:23 +0530

    Colors, FAQs see more button, footer, language support

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,5 @@
 var CURRENT_LANGUAGE = 'english';
+var LANGUAGES_METADATA;
 
 var getUrlParameter = function getUrlParameter(sParam) {
 	// https://stackoverflow.com/questions/19491336/get-url-parameter-jquery-or-how-to-get-query-string-values-in-js
@@ -18,7 +19,7 @@ var getUrlParameter = function getUrlParameter(sParam) {
 
 var smoothScroll = function () {
 	// when a navbar link is clicked, scroll to it
-	$("nav.navbar li a.nav-link").on('click', function (e) {
+	$(document).on('click', "nav.navbar li a.nav-link:not(.dropdown-toggle)", function (e) {
 		e.preventDefault();
 
 		var $this = $(this),
@@ -44,7 +45,7 @@ var faqSearch = function () {
   
   	$('#faq-search-bar').on('change keyup paste click', function () {
     	searchTerm = $(this).val();
-    	$('#faq-accordion > div.card').each(function () {
+    	$('#faq-accordion div.card').each(function () {
       		cardContainerId = '#' + $(this).attr('id');
       		$(cardContainerId + ':not(:containsCaseInsensitive(' + searchTerm + '))').hide();
       		$(cardContainerId + ':containsCaseInsensitive(' + searchTerm + ')').show();
@@ -57,11 +58,9 @@ var setCurrentLanguage = function (data) {
 		$languageSelector = $("#language-selector"),
 		language = getUrlParameter('language');
 
-	// set up the language choices available in the select menu on the top right of the page
-	supportedLanguages.forEach(function (value, i) {
-		var text = value[0].toUpperCase() + value.substring(1); // capitalize first letter
-		$languageSelector.append(`<option value="${value}">${text}</option>`);
-	});
+	if (language) {
+		language = language.toLowerCase();
+	}
 
 	// if the value was an unsupported language
 	// or if there was no "language" parameter at all
@@ -71,28 +70,7 @@ var setCurrentLanguage = function (data) {
 	}
 
 	// reaching here means we have some supported language for sure
-	
-	// set value of the selector
-	$languageSelector.val(language);
 	CURRENT_LANGUAGE = language;
-
-	// set up handler on the selection menu to load new page upon change
-	$languageSelector.on('change', function () {
-		location.search = '?language=' + $(this).val();
-		return;
-	});
-};
-
-var displayFAQs = function (language) {
-
-	$.getJSON(`assets/languages/${language}/faqs.json`, function(faqs) {
-		var source = $("#faq-template").html();
-		var template = Handlebars.compile(source);
-
-		var theCompiledHtml = template(faqs);
-		$("#faq-accordion").html(theCompiledHtml);
-	});
-
 };
 
 var simpleEventHandlers = function () {
@@ -101,25 +79,96 @@ var simpleEventHandlers = function () {
 	$(".graph-overlay").on('click', function() {
 		$(this).hide();
 	});
+
+	// toggle value of faq "See All button"
+	$(".faq-seeall-btn").click(function () {
+		var $this = $(this);
+		var value = ($this.text() == "Show All") ? "Show Less" : "Show All";
+		$this.text(value);
+	});
+};
+
+var updateLanguageOptions = function () {
+	var $languageSelector = $("#language-selector"),
+		supportedLanguages = LANGUAGES_METADATA.supported;
+
+	// set up the language choices available in the select menu on the top right of the page
+	supportedLanguages.forEach(function (value, i) {
+		var text = value[0].toUpperCase() + value.substring(1); // capitalize first letter
+		$languageSelector.append(`<option value="${value}">${text}</option>`);
+	});
+
+	// set value of the selector
+	$languageSelector.val(CURRENT_LANGUAGE);
+
+	// set up handler on the selection menu to load new page upon change
+	$languageSelector.on('change', function () {
+		location.search = '?language=' + $(this).val();
+		return;
+	});
+
+};
+
+var displayFile = function (language, fileIdentifier, containerSelector, callback) {
+	// loads assets/languages/<language>/<fileIdentifier>.txt
+	// loads template in #<fileIdentifier>-template
+	// displays HTML is #<containerSelector>
+	$.getJSON(`assets/languages/${language}/${fileIdentifier}.txt`, function(data) {
+		var source = $(`#${fileIdentifier}-template`).html();
+		var template = Handlebars.compile(source);
+
+		// file specific changes to object
+		if (fileIdentifier === 'faqs') {
+			// sort by "importance"
+			data = data.sort(function (a, b) {
+				var key1 = a.importance;
+				var key2 = b.importance;
+
+				if (key1 < key2) return -1;
+				else if (key2 < key1) return 1;
+				else return 0;
+			});
+			// insert a another parameter into the 10th item
+			// called "startHiding: true" for Handlebars
+			data[9].startHiding = true;
+			data[data.length - 1].endHiding = true;
+		}
+
+		var theCompiledHtml = template(data);
+		$(`${containerSelector}`).html(theCompiledHtml);
+		if (callback) {
+			callback();
+		}
+	});
 };
 
 var displayContent = function (CURRENT_LANGUAGE) {
-	// handle displaying of the FAQs
-	displayFAQs(CURRENT_LANGUAGE);
-
+	
+	// nav is loaded by default in english
+	// hence, we need to load this only if the language is anything else
 	if (CURRENT_LANGUAGE !== 'english') {
-		// displayAdvice();
+		displayFile(CURRENT_LANGUAGE, 'nav', '#nav-container', updateLanguageOptions);
 	}
-
-
+	
+	// FAQs and advice need to be loaded from the file even in English
+	// (cuz there are too many lol)
+	displayFile(CURRENT_LANGUAGE, 'advice', '#advice-container');
+	displayFile(CURRENT_LANGUAGE, 'faqs', '#faq-accordion');
 };
 
 $(document).ready(function () {
 	// handle the language selection
-	$.getJSON('assets/languages/metadata.json', function(data) {
+	$.getJSON('assets/languages/metadata.txt', function(data) {
+		LANGUAGES_METADATA = data;
 		
 		// set the CURRENT_LANGUAGE global
 		setCurrentLanguage(data);
+		// for english, we update the language options here itself
+		// for other languages, we first (re)load the nav, and then
+		// update the language options (in displayContent())
+		if (CURRENT_LANGUAGE == 'english') {
+			updateLanguageOptions();
+		}
 
 		// one the language has been set, display everything
 		displayContent(CURRENT_LANGUAGE);
